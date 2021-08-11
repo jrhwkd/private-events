1be06cd33ddbdc9ab2062ff3dd9a5c36604c876f
Jay <jrhwkd@gmail.com>
2021-08-11 11:39:46 -0500

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :users
+  root to: "home#index"
+  
+  resources :events do
+    resources :users
+  end
+
+  resources :users do
+    resources :events
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
