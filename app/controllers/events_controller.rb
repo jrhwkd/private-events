2ba1f62c12ae35845fe149b038b18d72ea156dff
Jay <jrhwkd@gmail.com>
2021-08-12 16:25:35 -0500

    working on views and submitting new event form with current user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :set_user
 
   # GET /events or /events.json
   def index
@@ -60,6 +62,10 @@ class EventsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
+    end
+
+    def set_user
+      @user = current_user
     end
 
     # Only allow a list of trusted parameters through.
